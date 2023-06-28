be9350ccfb4f10ff6b005e7fef214113e339bea5
kpyork <110615320+kpyork@users.noreply.github.com>
2023-06-28 23:39:54 +0530

    Add files via upload

---
+++ main/views.py
@@ -0,0 +1,32 @@
+from django.forms import SlugField
+from django.shortcuts import redirect, render, get_object_or_404
+from .models import Author, Category, Post
+from django.utils.text import slugify
+from .utils import update_views
+
+
+def home(request):
+    forums = Category.objects.all()
+    context = {
+        'forums': forums,
+    }
+    return render(request, "home.html", context)
+
+def posts(request):
+    category = get_object_or_404(Category, slug = slug)
+    posts = Post.objects.filter(approved = True,categories = category)
+
+    context = {
+        'posts': posts,
+        'forum' : category,
+        
+    }
+    return render(request, "posts.html", context)
+
+def detail(request):
+    post = get_object_or_404(Post, slug = slug)
+    context = {
+        'post': post,
+    }
+    update_views(request, post)
+    return render(request, "detail.html", context)
